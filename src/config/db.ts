ab053145bb94495de0408a5e55443ada38ed19ea
Ibrahim Mabrouki <ibrahim.mabrouki@lau.edu>
2026-02-24 12:20:17 +0200

    be structure

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,21 @@
-import mongoose from "mongoose";
+import { Client } from "pg";
+
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432, // PostgreSQL default port
+  password: process.env.DB_PASSWORD as string,
+  database: process.env.DATABASE as string,
+});
 
 const connectDB = async (): Promise<void> => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI as string);
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+  try {
+    await client.connect();
+    console.log("PostgreSQL connected");
+  } catch (error) {
+    console.error("Connection error:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,44 @@
 import dotenv from "dotenv";
 dotenv.config();
+import { prisma } from "./lib/prisma";
 
 import app from "./app";
 import connectDB from "./config/db";
 
-connectDB();
+// connectDB();
+
+
+prisma.$connect()
+  .then(() => {
+    console.log("✅ Database connected");
+  })
+  .catch((err) => {
+    console.error("❌ Database connection failed");
+    console.error(err);
+    process.exit(1);
+  });
+
+app.post("/users", async (req, res) => {
+    try {
+      const { name } = req.body;
+  
+      if (!name) {
+        return res.status(400).json({ error: "Name is required" });
+      }
+  
+      const newUser = await prisma.user.create({
+        data: {
+          name: name,
+        },
+      });
+  
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT as string}`);
 });
